d5f510b756815c960c0641957dca0b95af82c146
David Zemel <zemel96@gmail.com>
2021-04-05 19:25:54 -0400

    pokedex being displayed

---
+++ src/pokecard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import("./pokecard.css");
+
+function Pokecard(props) {
+  let { name, id, type, base_experience } = props;
+  let source = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  return (
+    <div>
+      <div>{name}</div>
+      <div>
+        <img src={source} alt=""></img>
+      </div>
+      <div>Type: {type}</div>
+      <div>EXP: {base_experience}</div>
+    </div>
+  );
+}
+
+export default Pokecard;
